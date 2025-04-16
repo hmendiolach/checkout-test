ed288565fe7c9dab5aa1d71c6ae546bdcf72321c
Hiram Mendiola <mendiolahiram@gmail.com>
2025-04-16 19:39:17 -0400

    ssl no

--- data-source.ts
+++ data-source.ts
@@ -1,15 +1,21 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
+  port: parseInt(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   synchronize: false,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/../migrations/**/*{.ts,.js}'],
+  logging: true,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/**/*{.ts,.js}'],
+  subscribers: ['src/subscribers/**/*{.ts,.js}'],
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
